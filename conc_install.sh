76b54ce76e6750c6a5fdbce2e42bec6716ff5bc1
SOMEONE <77111491+odbxtest@users.noreply.github.com>
2024-06-17 01:12:47 +0330

    Update conc_install.sh

--- conc_install.sh
+++ conc_install.sh
@@ -1,22 +1,5 @@
 #!/usr/bin/bash
 
-RED='\033[0;31m'
-GR='\033[0;32m'
-YE='\033[0;33m'
-NC='\033[0m'
-
-info() {
-    echo -e "${GR} $1 ${NC}"
-}
-
-warn() {
-    echo -e "${YE} $1 ${NC}"
-}
-
-error() {
-    echo -e "${RED} $1 ${NC}"
-}
-
 apt install sudo curl jq -y
 
 read -p "update and upgrade? (y/n): " updateQues
@@ -25,23 +8,23 @@ then
   apt-get update -y && apt-get upgrade -y
 fi
 
-getINFO=$(curl -s 'https://raw.githubusercontent.com/odbxtest/VAL2/main/info.json')
-concUrl=$(echo $getINFO | jq -r '.VAL2.url')
+getecho=$(curl -s 'https://raw.githubusercontent.com/odbxtest/VAL2/main/echo.json')
+concUrl=$(echo $getecho | jq -r '.VAL2.url')
 
-aptPacks=$(echo $getINFO | jq -r '.SERVER.apt[]')
+aptPacks=$(echo $getecho | jq -r '.SERVER.apt[]')
 aptCMD="sudo apt install -y"
 for aptPack in $aptPacks;do
   aptCMD="${aptCMD} $aptPack"
 done
-warn "${aptCMD}"
+echo "${aptCMD}"
 $aptCMD
 
-pipPacks=$(echo $getINFO | jq -r '.SERVER.pip[]')
+pipPacks=$(echo $getecho | jq -r '.SERVER.pip[]')
 pipCMD="pip3 install"
 for pipPack in $pipPacks;do
   pipCMD="${pipCMD} $pipPack"
 done
-warn "${pipCMD}"
+echo "${pipCMD}"
 $pipCMD
 
 pip3 install requests
@@ -54,38 +37,38 @@ if [[ $? != 0 ]];then
   chmod +x /etc/rc.local
   sudo chmod +x /usr/bin/badvpn-udpgw
   sudo screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7555 --max-clients 500
-  info "+ badvpn-udpgw installed"
-  warn "YOU MAY NEED TO REBOOT SERVER"
+  echo "+ badvpn-udpgw installed"
+  echo "YOU MAY NEED TO REBOOT SERVER"
 else
-  warn "\n- badvpn-udpgw already exist\n"
+  echo "\n- badvpn-udpgw already exist\n"
 fi
 
-bashFilesPath=$(echo $getINFO | jq -r '.BASH.path')
+bashFilesPath=$(echo $getecho | jq -r '.BASH.path')
 
 ls $bashFilesPath
 if [[ $? != 0 ]];then
   mkdir $bashFilesPath
-  info "\n+ Created dir [$bashFilesPath]\n"
+  echo "\n+ Created dir [$bashFilesPath]\n"
 fi
 
-bashFiles=$(echo $getINFO | jq -r '.BASH.files[]')
+bashFiles=$(echo $getecho | jq -r '.BASH.files[]')
 
 for file in $bashFiles;do
   cat $bashFilesPath$file >> /dev/null 2>&1
   if [[ $? != 0 ]];then
     sudo curl -L -o $bashFilesPath$file $concUrl/bash/$file
     sudo chmod +x $bashFilesPath$file
-    info "\n+ Added file [$file] to $bashFilesPath\n"
+    echo "\n+ Added file [$file] to $bashFilesPath\n"
   fi
 done
 
-sshPorts=$(echo $getINFO | jq -r '.SERVER.ssh_ports[]')
+sshPorts=$(echo $getecho | jq -r '.SERVER.ssh_ports[]')
 
 for port in $sshPorts;do
   sudo cat /etc/ssh/sshd_config | grep "Port $port" >> /dev/null 2>&1
   if [[ $? != 0 ]];then
     echo -e "\nPort $port" >> /etc/ssh/sshd_config
-    info "\n+ Added Port [$port] to sshd_config\n"
+    echo "\n+ Added Port [$port] to sshd_config\n"
   fi
 done
 
