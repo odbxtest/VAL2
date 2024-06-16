70ae58e83e74c570d6c253b294dee463320eb22c
SOMEONE <77111491+odbxtest@users.noreply.github.com>
2024-06-17 01:02:18 +0330

    Update conc_install.sh

--- conc_install.sh
+++ conc_install.sh
@@ -1,8 +1,47 @@
 #!/usr/bin/bash
 
-apt install sudo -y
-apt-get update -y && apt-get upgrade -y
-sudo apt install -y curl openssl jq screen iptables cron wget nano zip python3-pip
+RED='\033[0;31m'
+GR='\033[0;32m'
+YE='\033[0;33m'
+NC='\033[0m'
+
+info() {
+    echo -e "${GR}INFO${NC}: $1"
+}
+
+warn() {
+    echo -e "${YE}WARNING${NC}: $1"
+}
+
+error() {
+    echo -e "${RED}ERROR${NC}: $1" 1>&2
+    exit 1
+}
+
+
+
+apt install sudo curl jq -y
+
+read -p "update and upgrade? (y/n): " updateQues
+if [[ "$updateQues" == "y" || "$updateQues" == "Y" ]]
+then
+  apt-get update -y && apt-get upgrade -y
+fi
+
+getINFO=$(curl -s 'https://raw.githubusercontent.com/odbxtest/VAL2/main/info.json')
+concUrl=$(echo $getINFO | jq -r '.VAL2.url')
+
+aptPacks=$(echo $getINFO | jq -r '.SERVER.apt[]')
+for aptPack in $aptPacks;do
+  sudo apt install -y $aptPack
+done
+
+pipPacks=$(echo $getINFO | jq -r '.SERVER.pip[]')
+for pipPack in $pipPacks;do
+  sudo pip3 install $pipPack
+done
+
+
 pip3 install requests
 
 cat /usr/bin/badvpn-udpgw
@@ -13,22 +52,18 @@ if [[ $? != 0 ]];then
   chmod +x /etc/rc.local
   sudo chmod +x /usr/bin/badvpn-udpgw
   sudo screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7555 --max-clients 500
-  echo "- badvpn-udpgw installed"
-  echo "YOU MAY NEED TO REBOOT SERVER"
+  info "+ badvpn-udpgw installed"
+  warn "YOU MAY NEED TO REBOOT SERVER"
 else
-  echo "+ badvpn-udpgw already exist"
+  warn "\n- badvpn-udpgw already exist\n"
 fi
-
-getINFO=$(curl -s 'https://raw.githubusercontent.com/odbxtest/VAL2/main/info.json')
-
-concUrl=$(echo $getINFO | jq -r '.VAL2.url')
 
 bashFilesPath=$(echo $getINFO | jq -r '.BASH.path')
 
 ls $bashFilesPath
 if [[ $? != 0 ]];then
   mkdir $bashFilesPath
-  echo "+ Created dir [$bashFilesPath]"
+  info "\n+ Created dir [$bashFilesPath]\n"
 fi
 
 bashFiles=$(echo $getINFO | jq -r '.BASH.files[]')
@@ -38,7 +73,7 @@ for file in $bashFiles;do
   if [[ $? != 0 ]];then
     sudo curl -L -o $bashFilesPath$file $concUrl/bash/$file
     sudo chmod +x $bashFilesPath$file
-    echo "+ Added file [$file] to $bashFilesPath"
+    info "\n+ Added file [$file] to $bashFilesPath\n"
   fi
 done
 
@@ -48,7 +83,7 @@ for port in $sshPorts;do
   sudo cat /etc/ssh/sshd_config | grep "Port $port" >> /dev/null 2>&1
   if [[ $? != 0 ]];then
     echo -e "\nPort $port" >> /etc/ssh/sshd_config
-    echo "+ Added Port [$port] to sshd_config"
+    info "\n+ Added Port [$port] to sshd_config\n"
   fi
 done
 
