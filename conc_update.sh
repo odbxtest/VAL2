8aaf3463e3b13f6ba00cc44538238b6dd07f4a59
SOMEONE <77111491+odbxtest@users.noreply.github.com>
2025-10-07 22:57:11 +0330

    Update conc_update.sh

--- conc_update.sh
+++ conc_update.sh
@@ -66,10 +66,10 @@ conc_path=$(echo "$getConfiguration" | jq -r '.path')
 apt=$(echo "$getConfiguration" | jq -r '."apt"[]' 2>/dev/null)
 pip=$(echo "$getConfiguration" | jq -r '."pip"[]' 2>/dev/null)
 
-if [[ "$conc_awg_path" == *"/"* ]]; then
-    echo "OK - $conc_awg_path"
+if [[ "$conc_path" == *"/"* ]]; then
+    echo "OK - $conc_path"
 else
-    error "Error: Invalid or unsafe path '$conc_awg_path'."
+    error "Error: Invalid or unsafe path '$conc_path'."
 fi
 
 apt_wait
